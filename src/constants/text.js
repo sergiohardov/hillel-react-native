8dbd3bf243379a7aa78d6de7132ba143c24f6fdf
sergiohardov <sergiohardov@gmail.com>
2024-04-27 19:07:16 +0300

    Create constants

---
+++ src/constants/text.js
@@ -0,0 +1,28 @@
+const text = {
+  h1: {
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  h2: {
+    fontSize: 18,
+    // fontWeight: 'medium',
+  },
+  h3: {
+    fontSize: 16,
+    // fontWeight: 'medium',
+  },
+  p1: {
+    fontSize: 15,
+    // fontWeight: 'regular',
+  },
+  p2: {
+    fontSize: 14,
+    // fontWeight: 'regular',
+  },
+  body: {
+    fontSize: 12,
+    // fontWeight: 'regular',
+  },
+};
+
+export default text;
