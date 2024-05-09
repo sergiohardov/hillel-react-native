628584b981510c77320a8fcf1c1d8af0c858f597
sergiohardov <sergiohardov@gmail.com>
2024-05-09 12:17:33 +0300

    Update show screen

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet } from "react-native";
+
 import Home from "./src/screens/Home/Home";
+import ProductDetails from "./src/screens/ProductDetails/ProductDetails";
 
 import colors from "./src/constants/colors";
-import ProductDetails from "./src/screens/ProductDetails/ProductDetails";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-      {/* <Home /> */}
-      <ProductDetails />
+      <Home />
+      {/* <ProductDetails /> */}
     </SafeAreaView>
   );
 }
