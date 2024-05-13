0c3eda08c1b42e4b1b4fa8d85ddb28b50271bf44
sergiohardov <sergiohardov@gmail.com>
2024-05-13 20:03:53 +0300

    Updating mock data pizzalist

--- src/mock/pizzaList.js
+++ src/mock/pizzaList.js
@@ -1,4 +1,4 @@
-const pizzaList = [
+export const pizzaList = [
   {
     id: 1,
     title: "Margherita",
@@ -78,4 +78,138 @@ const pizzaList = [
   },
 ];
 
-export default pizzaList;
+export const updatePizzaList = [
+  {
+    id: 11,
+    title: "Mushroom Delight",
+    description:
+      "Savor the earthy goodness with mushrooms, mozzarella, and basil.",
+    price: "36.50",
+    image:
+      "https://png.pngtree.com/png-clipart/20230412/original/pngtree-modern-kitchen-food-boxed-cheese-lunch-pizza-png-image_9048155.png",
+  },
+  {
+    id: 12,
+    title: "Meat Feast",
+    description:
+      "Indulge in a carnivore's dream with pepperoni, sausage, ham, and mozzarella.",
+    price: "42.50",
+    image:
+      "https://png.pngtree.com/png-clipart/20231019/original/pngtree-pizza-png-with-ai-generated-png-image_13357740.png",
+  },
+  {
+    id: 13,
+    title: "Tropical Delight",
+    description:
+      "Experience a taste of the islands with ham, pineapple, and mozzarella.",
+    price: "39.50",
+    image: "https://freepngimg.com/save/6528-pizza-png-image/567x568",
+  },
+  {
+    id: 14,
+    title: "Mediterranean Mix",
+    description:
+      "Embark on a culinary journey with mozzarella, olives, sun-dried tomatoes, and feta.",
+    price: "44.50",
+    image:
+      "https://png.pngtree.com/png-clipart/20230412/original/pngtree-modern-kitchen-food-boxed-cheese-lunch-pizza-png-image_9048155.png",
+  },
+  {
+    id: 15,
+    title: "Cheese Galore",
+    description:
+      "Delight in a symphony of cheeses: mozzarella, cheddar, gorgonzola, and parmesan.",
+    price: "43.50",
+    image:
+      "https://png.pngtree.com/png-clipart/20231019/original/pngtree-pizza-png-with-ai-generated-png-image_13357740.png",
+  },
+  {
+    id: 16,
+    title: "Ocean's Bounty",
+    description:
+      "Dive into a sea of flavors with shrimp, calamari, and mozzarella.",
+    price: "46.50",
+    image: "https://freepngimg.com/save/6528-pizza-png-image/567x568",
+  },
+  {
+    id: 17,
+    title: "Garden Delight",
+    description:
+      "Enjoy the freshness of the garden with bell peppers, onions, mushrooms, and mozzarella.",
+    price: "38.50",
+    image:
+      "https://png.pngtree.com/png-clipart/20230412/original/pngtree-modern-kitchen-food-boxed-cheese-lunch-pizza-png-image_9048155.png",
+  },
+  {
+    id: 18,
+    title: "Creamy Chicken",
+    description:
+      "Delight in the richness of cream sauce, chicken, spinach, and mozzarella.",
+    price: "44.50",
+    image:
+      "https://png.pngtree.com/png-clipart/20231019/original/pngtree-pizza-png-with-ai-generated-png-image_13357740.png",
+  },
+  {
+    id: 19,
+    title: "Sweet & Smoky",
+    description:
+      "Satisfy your cravings with BBQ chicken, bacon, caramelized onions, and mozzarella.",
+    price: "45.50",
+    image: "https://freepngimg.com/save/6528-pizza-png-image/567x568",
+  },
+  {
+    id: 20,
+    title: "Spicy Fiesta",
+    description:
+      "Ignite your taste buds with spicy chicken, jalapenos, black beans, and avocado.",
+    price: "41.50",
+    image:
+      "https://png.pngtree.com/png-clipart/20230412/original/pngtree-modern-kitchen-food-boxed-cheese-lunch-pizza-png-image_9048155.png",
+  },
+];
+
+export const nextPizzaList = [
+  {
+    id: 21,
+    title: "Veggie Supreme",
+    description: "A medley of fresh vegetables atop a bed of mozzarella.",
+    price: "39.00",
+    image:
+      "https://png.pngtree.com/png-clipart/20230412/original/pngtree-modern-kitchen-food-boxed-cheese-lunch-pizza-png-image_9048155.png",
+  },
+  {
+    id: 22,
+    title: "Ultimate Meat Lover's",
+    description:
+      "A carnivore's delight loaded with sausage, bacon, ham, and pepperoni.",
+    price: "45.00",
+    image:
+      "https://png.pngtree.com/png-clipart/20231019/original/pngtree-pizza-png-with-ai-generated-png-image_13357740.png",
+  },
+  {
+    id: 23,
+    title: "Tropical Fusion",
+    description:
+      "Tangy pineapple and succulent ham meet for a tropical taste sensation.",
+    price: "42.00",
+    image: "https://freepngimg.com/save/6528-pizza-png-image/567x568",
+  },
+  {
+    id: 24,
+    title: "Mediterranean Magic",
+    description:
+      "Savor the flavors of the Mediterranean with olives, feta, and sun-dried tomatoes.",
+    price: "46.00",
+    image:
+      "https://png.pngtree.com/png-clipart/20230412/original/pngtree-modern-kitchen-food-boxed-cheese-lunch-pizza-png-image_9048155.png",
+  },
+  {
+    id: 25,
+    title: "Cheese Extravaganza",
+    description:
+      "A cheese lover's dream featuring a blend of four exquisite cheeses.",
+    price: "44.00",
+    image:
+      "https://png.pngtree.com/png-clipart/20231019/original/pngtree-pizza-png-with-ai-generated-png-image_13357740.png",
+  },
+];
