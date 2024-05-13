a3b1941f73a4c4406addea20bc2252dbdeddda23
sergiohardov <sergiohardov@gmail.com>
2024-05-13 20:04:25 +0300

    Refactor & add new fetures for list

--- src/screens/ProductDetails/ProductDetails.jsx
+++ src/screens/ProductDetails/ProductDetails.jsx
@@ -10,10 +10,36 @@ import List from "./components/List/List";
 import ProductDetailsContext from "../../contexts/ProductDetails";
 
 export default function ProductDetails() {
+  const [listLoading, setListLoading] = useState(false);
+  const [refreshLoading, setRefreshLoading] = useState(false);
+  const [additionalLoading, setAdditionalLoading] = useState(false);
+
+  const [listData, setListData] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
+
   const [inputValue, setInputValue] = useState("");
+  const [page, setPage] = useState(1);
 
   return (
-    <ProductDetailsContext.Provider value={{ inputValue, setInputValue }}>
+    <ProductDetailsContext.Provider
+      value={{
+        listLoading,
+        setListLoading,
+        refreshLoading,
+        setRefreshLoading,
+        additionalLoading,
+        setAdditionalLoading,
+
+        listData,
+        setListData,
+        filteredList,
+        setFilteredList,
+        inputValue,
+        setInputValue,
+        page,
+        setPage,
+      }}
+    >
       <View style={styles.container}>
         <TopBar />
         <Title />
