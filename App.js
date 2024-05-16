9a327167f9615d83d01965cba78c29ab1876a2ed
sergiohardov <sergiohardov@gmail.com>
2024-05-16 15:23:12 +0300

    Add colorSheme in context & refactor styles

--- App.js
+++ App.js
@@ -1,26 +1,31 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet, useColorScheme } from "react-native";
 
 import Home from "./src/screens/Home/Home";
 import ProductDetails from "./src/screens/ProductDetails/ProductDetails";
 
 import AppContext from "./src/contexts/App";
+import theme from "./src/constants/theme";
 
 export default function App() {
+  const colorSheme = useColorScheme();
+  const propStyles = styles(colorSheme);
+
   return (
-    <AppContext.Provider value={{  }}>
-    <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
-      {/* <Home /> */}
-      <ProductDetails />
-    </SafeAreaView>
+    <AppContext.Provider value={{ colorSheme }}>
+      <SafeAreaView style={propStyles.container}>
+        <StatusBar style="auto" />
+        {/* <Home /> */}
+        <ProductDetails />
+      </SafeAreaView>
     </AppContext.Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.seasalt,
-  },
-});
+const styles = (colorSheme) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme[colorSheme].primaryBackground,
+    },
+  });
