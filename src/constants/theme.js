5edef0a6a9d2c3139a4dc10007c33ee8f625ede2
sergiohardov <sergiohardov@gmail.com>
2024-05-16 14:31:42 +0300

    Add theme colors

---
+++ src/constants/theme.js
@@ -0,0 +1,15 @@
+import colors from "./colors";
+
+const dark = {
+  primary: colors.white,
+  secondary: colors.black,
+  primaryBackground: colors.gray_dark,
+};
+
+const light = {
+  primary: colors.black,
+  secondary: colors.white,
+  primaryBackground: colors.seasalt,
+};
+
+export default { dark, light };
