6e22bc4f03e1d074a54f00eb454030aecbedda6a
sergiohardov <sergiohardov@gmail.com>
2024-05-21 19:54:13 +0300

    Update home screen

--- src/screens/Home/HomeScreen.jsx
+++ src/screens/Home/HomeScreen.jsx
@@ -1,13 +1,16 @@
-import { SafeAreaView, View } from "react-native";
+import { SafeAreaView, ScrollView } from "react-native";
+import TopBar from "./components/TopBar/TopBar";
+import Slider from "../../components/Slider/Slider";
 
 import styles from "./styles";
 
-import Slider from "../../components/Slider/Slider";
-
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Slider />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <TopBar navigation={navigation} />
+        <Slider />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
